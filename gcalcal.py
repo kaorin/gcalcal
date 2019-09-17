bf986558bddffd2b01a519bfcac80b7ccaad4e7e
kaoru.konno <kaoru.konno@gmail.com>
2019-09-17 18:14:11 +0900

    修正

--- gcalcal.py
+++ gcalcal.py
@@ -112,8 +112,8 @@ class myCalendar:
         self.lblYear = self.wMain.get_object ("lblYear")
         lblNo = 0
         self.days = [[0 for i in range(7)] for j in range(7)]
-        for row in range(0,6):
-            for col in range(0,7):
+        for row in range(6):
+            for col in range(7):
                 self.days[row][col] = self.wMain.get_object ("lbl{:02d}".format(lblNo))
                 lblNo += 1
         # GdkColormap to GdkVisual
@@ -126,16 +126,9 @@ class myCalendar:
             print ("no Composited...")
         dic = {
             "on_miExit_activate" : self.on_miExit_activate,
-            "on_MainWindow_destroy" : self.on_MainWindow_destroy,
-            "on_MainWindow_button_press_event" : self.on_MainWindow_button_press_event,
-            "on_calCalender_day_selected" : self.on_calCalender_day_selected,
-            "on_calCalender_day_selected_double_click" : self.on_calCalender_day_selected_double_click,
-            "on_calCalender_month_changed" : self.on_calCalender_month_changed,
-            "on_calCalender_next_month" : self.on_calCalender_month_changed,
-            "on_calCalender_prev_month" : self.on_calCalender_month_changed,
-            "on_calCalender_next_year" : self.on_calCalender_month_changed,
-            "on_calCalender_prev_year" : self.on_calCalender_month_changed,
-            "on_MainWindow_realize" : self.on_MainWindow_realize,
+            "on_wCalendar_destroy" : self.on_MainWindow_destroy,
+            "on_wCalendar_button_press_event" : self.on_MainWindow_button_press_event,
+            "on_wCalendar_realize" : self.on_MainWindow_realize,
             "on_miTitlebar_toggled" : self.on_miTitlebar_toggled,
             "on_mi010_activate" : self.on_miOpacity_activate,
             "on_mi020_activate" : self.on_miOpacity_activate,
@@ -148,10 +141,10 @@ class myCalendar:
             "on_mi090_activate" : self.on_miOpacity_activate,
             "on_mi100_activate" : self.on_miOpacity_activate,
             "on_MainWindow_focus_out_event": self.on_MainWindow_focus_out_event,
-            "on_lblMonthDown_button_release_event": self.on_lblMonthDown_button_release_event,
-            "on_lblMonthUp_button_release_event": self.on_lblMonthUp_button_release_event,
-            "on_lblYearDonw_button_release_event": self.on_lblYearDonw_button_release_event,
-            "on_lblYearUp_button_release_event": self.on_lblYearUp_button_release_event,
+            "on_evMonthDown_button_release_event": self.on_evMonthDown_button_release_event,
+            "on_evMonthUp_button_release_event": self.on_evMonthUp_button_release_event,
+            "on_evYearDonw_button_release_event": self.on_evYearDonw_button_release_event,
+            "on_evYearUp_button_release_event": self.on_evYearUp_button_release_event,
         }
         self.wMain.connect_signals(dic)
         xpos = conf.GetOption("x_pos")
@@ -175,9 +168,13 @@ class myCalendar:
         # self.setEventDay()
         self.set_style()
         self.makeCalendar(now.year, now.month)
+        self.setHoliday(16)
+        self.setHoliday(23)
+        self.setHoliday(30)
         self.mainWindow.show_all()
 
     def makeCalendar(self,year,month):
+        self.initDayStyle()
         calendar.setfirstweekday(calendar.SUNDAY)
         self.lblMonth.set_text(calendar.month_name[month])
         self.lblYear.set_text(str(year))
@@ -192,26 +189,84 @@ class myCalendar:
             start = 1
         calRow = 0
         for row in range(start, 6):
-            for col in range(0,7):
+            for col in range(7):
                 if cal[calRow][col] != 0:
                     self.days[row][col].set_text(str(cal[calRow][col]))
                     if date.today() == date(year, month, cal[calRow][col]):
                         css_context = self.days[row][col].get_style_context()
                         css_context.add_class("today")
-
             calRow += 1
+            if len(cal) <= calRow:
+                break
+        # 前月
         prevLastRow = len(prevcal)
-        for col in range(0,7):
+        for col in range(7):
             if prevcal[prevLastRow - 1][col] != 0:
                 self.days[0][col].set_text(str(prevcal[prevLastRow - 1][col]))
                 css_context = self.days[0][col].get_style_context()
                 css_context.add_class("prev_month")
-        for col in range(0,7):
-            if nextcal[0][col] != 0:
-                self.days[5][col].set_text(str(nextcal[0][col]))
-                css_context = self.days[5][col].get_style_context()
-                css_context.add_class("next_month")
+        # 次月
+        lastrow = len(cal) - 1 + start
+        start = 0
+        if firstWeek != 6:
+            start = 1
+        for row in range(6 - len(cal) + start):
+            for col in range(7):
+                if nextcal[row][col] != 0:
+                    self.days[lastrow+row][col].set_text(str(nextcal[row][col]))
+                    css_context = self.days[lastrow+row][col].get_style_context()
+                    css_context.add_class("next_month")
         
+    def initDayStyle(self):
+        for row in range(6):
+            for col in range(7):
+                css_context = self.days[row][col].get_style_context()
+                css_context.remove_class("today")
+                css_context.remove_class("holiday")
+                css_context.remove_class("prev_month")
+                css_context.remove_class("next_month")
+                css_context.remove_class("selected")
+                css_context.remove_class("marked")
+
+    def findDayLabel(self, day):
+        for row in range(6):
+            for col in range(7):
+                css_context = self.days[row][col].get_style_context()
+                if css_context.has_class("prev_month") == False and css_context.has_class("next_month") == False:
+                    if int(self.days[row][col].get_text()) == day:
+                        return self.days[row][col]
+        return None
+
+    def setHoliday(self, day):
+        day = self.findDayLabel(day)
+        css_context = day.get_style_context()
+        css_context.add_class("holiday")
+        day.set_has_tooltip(True)
+        day.set_tooltip_text("holiday")
+
+
+    def on_evMonthDown_button_release_event(self, wdget, event):
+        prevdate = date(self.year,self.month,1) - timedelta(days=1)
+        self.month = prevdate.month
+        self.makeCalendar(self.year, self.month)
+        return
+
+    def on_evMonthUp_button_release_event(self, wdget, event):
+        firstWeek, lastday = calendar.monthrange(self.year, self.month)
+        nextdate = date(self.year,self.month,lastday) + timedelta(days=1)
+        self.month = nextdate.month
+        self.makeCalendar(self.year, self.month)
+        return
+
+    def on_evYearDonw_button_release_event(self, wdget, event):
+        self.year -= 1
+        self.makeCalendar(self.year, self.month)
+        return
+
+    def on_evYearUp_button_release_event(self, wdget, event):
+        self.year += 1
+        self.makeCalendar(self.year, self.month)
+        return
 
 
     def on_MainWindow_button_press_event(self,widget,event):
